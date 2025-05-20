f2ad8a8ab8956b5f0aa8fd7ee003c59cfc1b8ed1
SJ <ligerssj@naver.com>
2025-05-20 10:39:49 +0900

    생성자 오버로딩

---
+++ chap06/src/sec03/exam03/quiz/Animal.java
@@ -0,0 +1,26 @@
+package sec03.exam03.quiz;
+
+public class Animal {
+	String kind;
+	int age;
+
+	Animal(String kind) {
+		this.kind = kind;
+		this.age = 1;
+	}
+
+	Animal(int age) {
+		this.kind = "사람";
+		this.age = age;
+	}
+
+	Animal(String kind, int age) {
+		this.kind = kind;
+		this.age = age;
+	}
+
+	// 출력 메소드
+	void info() {
+		System.out.printf("%s: %d살%n", kind, age);
+	}
+}
