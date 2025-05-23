fb156006699f9cbad5924be1945dd27f896ccff9
SJ <ligerssj@naver.com>
2025-05-23 12:52:02 +0900

    추상 클래스

---
+++ chap07/src/sec02/exam06/InstanceOfExample.java
@@ -0,0 +1,43 @@
+package sec02.exam06;
+
+public class InstanceOfExample {
+
+	public static void main(String[] args) {
+		// 테스트 먼저
+		Parent parent = new Parent();
+//		Child child = (Child) parent; // ClassCastException 예외 발생
+		// 강제 타입 변환을 하기 전에 검사(안전한 코딩)
+		// 검사할 객체(좌측) instanceof 클래스 타입(우측)
+		if (parent instanceof Child) { // parent 변수가 참조하는 객체가 Child 타입인지 검사
+			Child child = (Child) parent; // 강제 타입 변환
+			System.out.println("강제 타입 변환 성공");
+		}
+		parent = new Child();
+		// 자바 12부터 추가된 문법
+		// instanceof의 결과가 true일 경우 자동 타입 변환되어 변수에 대입
+		if (parent instanceof Child child) {
+			System.out.println("강제 타입 변환 성공");
+			System.out.println(child);
+		}
+
+		Parent parentA = new Child();
+		method1(parentA);
+
+		Parent parentB = new Parent();
+		method1(parentB);
+	}
+
+	public static void method1(Parent parent) {
+		// 안전하게 타입 검사 후 변환하기
+		System.out.println(parent.getClass());
+		System.out.println(parent.getClass().getName());
+		if (parent instanceof Child child) {
+//			Child child = (Child) parent;
+			System.out.println("method1 - Child로 변환 성공");
+			System.out.println(child);
+		} else {
+			System.out.println("변환할 수 없음");
+		}
+	}
+
+}
