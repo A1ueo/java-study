37fcd21825e430041c5d6e21a2c8bd26f73d9edf
SJ <ligerssj@naver.com>
2025-05-29 18:09:27 +0900

    문자열 메소드 endWith()

--- chap11/src/homework/homework02/Practice.java
+++ chap11/src/homework/homework02/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
 	public static String takeState(String str) {
 		String[] strArr = str.split(" ");
 		for(String s : strArr) {
+//			if(s.endsWith("구"))
 			if(s.lastIndexOf("구") == s.length() - 1)
 				return s;
 		}
